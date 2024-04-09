32ce269023b691959fd8a60e0959bba7b416fc24
Mouna GUELSA <86554888+MounaGuelsa@users.noreply.github.com>
2024-04-09 01:04:02 +0000

    rapports and statistics1 on

---
+++ RevenueMicroservice/src/main/java/org/money/revenuemicroservice/exceptions/CustomException.java
@@ -0,0 +1,19 @@
+package org.money.revenuemicroservice.exceptions;
+
+import lombok.Getter;
+
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public CustomException(String message, HttpStatus status) {
+        super(message);
+        this.status=status;
+    }
+
+
+
+}
